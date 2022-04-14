394e8dbaf703dd06a6a105b86f57f493730ad9fc
Tony Linz <tonyllinz@gmail.com>
2022-04-14 18:36:36 -0500

    more file structure

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import Header from "./components/About";
+import Nav from "./components/Nav";
+import Project from "./components/Gallery";
+import Footer from "./components/Contact";
+import ContactForm from "./components/Contact";
+
+function App() {
+    return <div>{/* components go here */}</div>;
+}
+
+export default App;
