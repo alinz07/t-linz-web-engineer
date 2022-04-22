6a2e737b295369210da7176ecd64a6f466f5a645
Tony Linz <tonyllinz@gmail.com>
2022-04-22 00:01:43 -0500

    finish about me and resume css. start projects css

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,25 +1,34 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
+import { Avatar } from "@mui/material";
 
 function About() {
     return (
-        <section className="body-section" id="about-me">
-            <h2 className="section-title">About Me</h2>
-
-            <img
-                src={require("../../assets/Profile Picture.PNG")}
-                alt="me, Tony Linz"
-                key="profile-picture"
-            />
-
-            <div>
-                <p>
-                    Full-Stack Web Development Bootcamp graduate from UWM.
-                    Hoping to contribute to a company that cares about CSR and
-                    encourages me to leverage my problem-solving and leadership
-                    skills while exploring my passion for tech.
-                </p>
-            </div>
-        </section>
+        <Grid
+            container
+            direction="column"
+            justify-content="center"
+            alignItems="center"
+            id="react-card"
+            wrap="wrap"
+        >
+            <Grid item p={2} fontSize={25}>
+                <h2 id="about-title">About Me</h2>
+            </Grid>
+            <Grid item p={2}>
+                <Avatar
+                    src={require("../../assets/Profile Picture.PNG")}
+                    alt="me, Tony Linz"
+                    sx={{ width: 150, height: 150 }}
+                />
+            </Grid>
+            <Grid item id="about-para" p={2}>
+                Full-Stack Web Development Bootcamp graduate from UWM. Hoping to
+                contribute to a company that cares about CSR and encourages me
+                to leverage my problem-solving and leadership skills while
+                exploring my passion for tech.
+            </Grid>
+        </Grid>
     );
 }
 
