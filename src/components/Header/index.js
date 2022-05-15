f00da9f9060d29e23385a0041f6f1a9855af131f
Tony Linz <tonyllinz@gmail.com>
2022-05-14 23:55:24 -0500

    link home board

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -14,10 +14,12 @@ function Header(props) {
             alignItems="center"
             id="header"
         >
-            <Grid item xs={12} id="hero" mt={3}>
-                <h1 id="my-name">Tony Linz</h1>
-                <h2 id="subtitle">Full Stack Web Engineer</h2>
-            </Grid>
+            <a href="/" id="nameboard-home-link">
+                <Grid item xs={12} id="hero" mt={3}>
+                    <h1 id="my-name">Tony Linz</h1>
+                    <h2 id="subtitle">Full Stack Web Engineer</h2>
+                </Grid>
+            </a>
             <Grid item xs={12} mt={3} mb={10}>
                 <Nav
                     setCurrentNav={setCurrentNav}
