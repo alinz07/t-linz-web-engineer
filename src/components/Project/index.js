9d8bf5128326f8115c8673ecf4fb5013ef312a84
Tony Linz <tonyllinz@gmail.com>
2022-05-18 12:36:30 -0500

    update project descriptions.

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -11,7 +11,7 @@ function Project() {
             alt: "Propagatorz homepage",
             repo: "https://github.com/alinz07/Propagatorz",
             aitchref: "https://propagatorz-plant-help.herokuapp.com/",
-            desc: "A social media solution for users to connect with other plant enthusiasts for all their plant questions or to show off their plant babies.",
+            desc: "A social media solution for users to connect with other plant enthusiasts for all their plant questions or to show off their plant babies. My contributions were on the back and front-end. I set up the express server to connect to MongoDB, Mongoose, GraphQL and Apollo Server, set up global state, actions and reducers, wrote mutations and queries, added CSS and coded the post and update post form components.",
         },
         {
             name: "Photo portfolio",
@@ -20,7 +20,7 @@ function Project() {
             alt: "Photo portfolio homepage",
             repo: "https://github.com/alinz07/photo-port",
             aitchref: "https://alinz07.github.io/photo-port/",
-            desc: "A mock Photographer's portfolio leveraging React and a suite of Jest tests for the components",
+            desc: "A mock Photographer's portfolio leveraging React and a suite of Jest tests for the components. I contributed by converting this to a React app. The HTML, CSS and most Javascript logic was already written by the University.",
         },
         {
             name: "Budget Tracker",
@@ -29,7 +29,7 @@ function Project() {
             alt: "Budget Tracker homepage",
             repo: "https://github.com/alinz07/budget-tracker-mod19",
             aitchref: "https://budget-tracker-mod19-tlinz.herokuapp.com/",
-            desc: "A Progressive Web Application that uses a MongoDB database, Mongoose for ORM and IndexedDB for data persistence.",
+            desc: "A Progressive Web Application that uses a MongoDB database, Mongoose for ORM and IndexedDB for data persistence. My contributions were all on the back end, the front end was already set up by University code. I also converted this to a PWA.",
         },
         {
             name: "Bookworm Bookclub",
@@ -38,7 +38,7 @@ function Project() {
             alt: "Bookworm homepage",
             repo: "https://github.com/Hmlessard/Bookworm",
             aitchref: "https://asgardians-bookworm.herokuapp.com/",
-            desc: "A bookclub web application that allows users to discuss their favorite reads, add new books to their list of books to read and delete them when done. This application's codebase was built using the MVC paradigm, using MySQL and the Sequelize ORM for the database, Handlebars.js for the views, and Express and Express Session with cookies for the RESTful APIs with authentication. Login username: Bilbo  Password: Precious",
+            desc: "A bookclub web application that allows users to post about and comment on posts for their favorite reads, add new books to their list of books to read and delete them when done. This application's codebase was built using the MVC paradigm, MySQL and the Sequelize ORM for the database, Handlebars.js for the views, and Express and Express Session with cookies for the RESTful APIs with authentication, My contributions were on the back and front-end: I wrote our RESTful controllers, connected the bookcover API and wrote logic to use it, added CSS, added to multiple handlebars pages in the views, and set up the db. Login username: Bilbo  Password: Precious",
         },
         {
             name: "National Parks Info and Weather",
@@ -48,7 +48,7 @@ function Project() {
             repo: "https://github.com/p-fassbender/national-parks-info-and-weather",
             aitchref:
                 "https://p-fassbender.github.io/national-parks-info-and-weather/",
-            desc: "An application for hikers to view national parks in their state and a 5 day weather forecast to help them plan an adventure. My contributions include front and back-end development with a focus on dynamic HTML and parsing API JSON data.",
+            desc: "An application for hikers to view national parks in their state and a 5 day weather forecast to help them plan an adventure. My contributions included dynamic HTML scripts and CSS, as well as parsing API JSON data for the National Parks and Weather API.",
         },
         {
             name: "Ton's Tech Blog",
@@ -57,7 +57,7 @@ function Project() {
             alt: "Ton's Tech Blog homepage",
             repo: "https://github.com/alinz07/mvc-techblog",
             aitchref: "https://tons-tech-blog.herokuapp.com/",
-            desc: "A social media platform that follows the MVC paradigm using SQL and Sequelize, Handlebars.js and Express-Session with cookies for user authentication. Login username: Bilbo  Password: Precious",
+            desc: "A social media platform that follows the MVC paradigm using SQL and Sequelize, Handlebars.js and Express-Session with cookies for user authentication. I was the sole contributor. Login username: Bilbo  Password: Precious",
         },
     ];
     return (
