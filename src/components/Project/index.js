6e1423b4bf82a6a24420bc9e4d27b9c826f18cea
Tony Linz <tonyllinz@gmail.com>
2025-11-19 16:22:18 -0800

    update linz ling exercises text

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -14,13 +14,13 @@ function Project() {
 			desc: "A static web page for a client's mobile charcuterie bar business that leverages smoothe rendering from vites lazy loading.",
 		},
 		{
-			name: "Linz-Linz Exercises",
+			name: "Linz-Ling Exercises",
 			tech: "MongoDB Atlas, Express, React, Node, Javascript",
 			imgName: "linzling.png",
 			alt: "linzling logged in page",
 			repo: "https://github.com/alinz07/mern-deploy",
 			aitchref: "https://mern-deploy-1-hixt.onrender.com/",
-			desc: "Still in development, this appliaction is to help my sister who is a teach for the deaf and hard of hearing. It's a MERN app that helps track her students' daily audio equipment checks.",
+			desc: "Still in development, this appliaction is to help my sister who is a teacher for the deaf and hard of hearing. It's a MERN app that helps track her students' daily audio equipment checks.",
 		},
 		{
 			name: "National Parks Info and Weather",
