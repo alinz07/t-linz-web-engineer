557df3d6775b86f20aa5ebe745faefb2a932b3a4
Tony Linz <tonyllinz@gmail.com>
2025-11-19 16:34:29 -0800

    update about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,43 +3,41 @@ import Grid from "@mui/material/Grid";
 import { Avatar } from "@mui/material";
 
 function About() {
-    return (
-        <Grid
-            container
-            direction="column"
-            justify-content="center"
-            alignItems="center"
-            id="react-card"
-            wrap="wrap"
-            mx="auto"
-            my={9}
-        >
-            <Grid item p={2} fontSize={25}>
-                <h2 id="about-title">About Me</h2>
-            </Grid>
-            <Grid item p={2}>
-                <Avatar
-                    src={require("../../assets/Profile Picture.PNG")}
-                    alt="me, Tony Linz"
-                    sx={{ width: 150, height: 150 }}
-                />
-            </Grid>
-            <Grid item id="about-para" p={2}>
-                Full-Stack Web Engineer with a Certificate in Full-Stack Coding
-                from the University of Wisconsin-Milwaukee (May 2022), with a
-                background in Compliance and spoken Languages and experience
-                creating applications with MongoDB, Express, React and Node.
-                Hoping to contribute to a sustainable company that cares about
-                corporate-social responsibility and encourages team members to
-                explore their passions for tech. In addition to my newly
-                developed full-stack skills and enthusiasm for Python
-                algorithms, I’m looking to leverage my strong problem-solving,
-                communication, teamwork and leadership skills gained from
-                leading a Team of six auditors, professionally, and 300+
-                volunteers for American Cancer Society annual fundraisers.
-            </Grid>
-        </Grid>
-    );
+	return (
+		<Grid
+			container
+			direction="column"
+			justify-content="center"
+			alignItems="center"
+			id="react-card"
+			wrap="wrap"
+			mx="auto"
+			my={9}
+		>
+			<Grid item p={2} fontSize={25}>
+				<h2 id="about-title">About Me</h2>
+			</Grid>
+			<Grid item p={2}>
+				<Avatar
+					src={require("../../assets/Profile Picture.PNG")}
+					alt="me, Tony Linz"
+					sx={{ width: 150, height: 150 }}
+				/>
+			</Grid>
+			<Grid item id="about-para" p={2}>
+				Experienced Full Stack Engineer with a background in compliance
+				and spoken languages. I'm looking to contribute to sustainable
+				companies that care about corporate-social responsibility and
+				encourage team members to explore their passions for tech. In
+				addition to my full stack skills, professional Salesforce
+				experience and enthusiasm for Python algorithms, I’m looking to
+				leverage my strong problem-solving, communication, teamwork and
+				leadership skills gained from leading a team of six auditors
+				professionally, and 300+ volunteers for American Cancer Society
+				annual fundraisers.
+			</Grid>
+		</Grid>
+	);
 }
 
 export default About;
